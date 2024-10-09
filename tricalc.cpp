94429254033dbbbfd3818c9d9677e4d69d5e19a8
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-09 19:22:06 +0200

    Update tricalc.cpp
    Logo w trzech kolorach dodane

--- tricalc.cpp
+++ tricalc.cpp
@@ -5,14 +5,21 @@ using namespace std;
 HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
 void logo()
 {
-    SetConsoleTextAttribute(color,12);
+    SetConsoleTextAttribute(color,9);
     cout<<R"(
-    [Tu bedzie logo]
-   )";
+  mm              db   .g8"""bgd         `7MM
+  MM                 .dP'     `M           MM                                       __,
+mmMMmm `7Mb,od8 `7MM dM'       ` ,6"Yb.    MM  ,p6"bo      `7M'   `MF',pP""Yq.     `7MM  )"<<endl;SetConsoleTextAttribute(color,13);
+cout<<R"(  MM     MM' "'   MM MM         8)   MM    MM 6M'  OO        VA   ,V 6W'    `Wb      MM
+  MM     MM       MM MM.         ,pm9MM    MM 8M              VA ,V  8M      M8      MM)"<<endl;SetConsoleTextAttribute(color,14);
+cout<<R"(  MM     MM       MM `Mb.     ,'8M   MM    MM YM.    ,         VVV   YA.    ,A9 ,,   MM
+  `Mbmo.JMML.   .JMML. `"bmmmd' `Moo9^Yo..JMML.YMbmd'           W     `Ybmmd9'  db .JMML.
+
+)";
+SetConsoleTextAttribute(color,7);
 }
 int main()
 {
     logo();
     return 0;
 }
-
