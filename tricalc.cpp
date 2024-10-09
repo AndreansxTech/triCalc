ad6e782f055966e923b94d8bbc481aeff9548ddc
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-09 20:12:00 +0200

    Update tricalc.cpp
    Zaczynam menu wprowadzania danych

--- tricalc.cpp
+++ tricalc.cpp
@@ -3,6 +3,12 @@
 #include <cmath>
 using namespace std;
 HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
+char wybor;
+int kolor;
+void kolory()
+{
+    SetConsoleTextAttribute(color,kolor);
+}
 void logo()
 {
     SetConsoleTextAttribute(color,9);
@@ -18,8 +24,46 @@ cout<<R"(  MM     MM       MM `Mb.     ,'8M   MM    MM YM.    ,         VVV   YA
 )";
 SetConsoleTextAttribute(color,7);
 }
+void trojkat()
+{
+    cout<<R"(
+
+    )";
+}
+void informacja()
+{
+    kolor=14;kolory();cout<<"[";
+    kolor=12;kolory();cout<<"!";
+    kolor=14;kolory();cout<<"] ";
+}
+void menu()
+{
+    informacja();
+    cout<<"Pamietaj o zasadach wprowadzania danych: \n";
+    kolor=12;kolory();cout<<" kat alfa";kolor=14;kolory();cout<<" znajduje sie pomiedzy bokami a i c, \n";
+    kolor=12;kolory();cout<<" kat beta";kolor=14;kolory();cout<<" jest miedzy bokami a i b, \n";
+    kolor=12;kolory();cout<<" kat gamma";kolor=14;kolory();cout<<" jest pomiedzy bokami b i c\n";
+    kolor=11;kolory();cout<<"Ktory bok chcesz obliczyc?\n";
+    cout<<"Wpisz odpowiednio 1 dla boku a, 2 dla boku b, 3 dla boku c, a dla katu alfa, b dla katu beta, c dla katu gamma: ";
+    kolor=9;kolory();cin>>wybor;
+}
 int main()
 {
     logo();
+    while (true)
+    {
+    menu();
+        switch(wybor)
+        {
+        case '1':
+            informacja(); cout<<" Wybrales obliczanie boku a";kolor=11;kolory();
+            cout<<" Ktory kat trojkata chcesz wprowadzic";
+            break;
+        default:
+            kolor=12;kolory();cout<<" Nie ma takiej opcji\n";
+            break;
+        }
+    }
+
     return 0;
 }
