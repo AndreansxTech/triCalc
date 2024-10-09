d9ab007bc3ce2af9aa05ecdbf938090012bab132
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-09 22:42:10 +0200

    Update tricalc.cpp
    Teraz w pełni działa zapis danych. Do zrobienia jeszcze całe liczenie oraz warunki

--- tricalc.cpp
+++ tricalc.cpp
@@ -3,9 +3,9 @@
 #include <cmath>
 using namespace std;
 HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
-char wybor, wybor2;
-float alfa,beta,gamma;
-int kolor, juz_zapisane;
+char wybor, wybor2,wybor3;
+float alfa,beta,gamma,bok_a,bok_b,bok_c;
+int kolor,juz_zapisane,katy,boki;;
 void kolory()
 {
     SetConsoleTextAttribute(color,kolor);
@@ -38,19 +38,29 @@ void informacja()
 }
 void menu()
 {
+
     informacja();
     cout<<"Pamietaj o zasadach wprowadzania danych: \n";
     kolor=12;kolory();cout<<" kat alfa";kolor=14;kolory();cout<<" znajduje sie pomiedzy bokami c i b, \n";
     kolor=12;kolory();cout<<" kat beta";kolor=14;kolory();cout<<" jest miedzy bokami a i c, \n";
     kolor=12;kolory();cout<<" kat gamma";kolor=14;kolory();cout<<" jest pomiedzy bokami a i b\n";
     kolor=11;kolory();
-    cout<<" Musisz zadeklarowc trzy dane aby obliczyc wszystkie boki trojkata\n";
-    int zadeklarowane_katy;
-    while (juz_zapisane<3)
-    {
-        cout<<" Jak narazie zadeklarowales "<<juz_zapisane<<" danych\n";
-        if(zadeklarowane_katy<2)
+    cout<<" Musisz zadeklarowc trzy dane aby obliczyc wszystkie boki trojkata\n Kiedy zadeklarujesz trzy dane program obliczy reszte\n";
+
+
+
+}
+
+
+int main()
+{
+    juz_zapisane,katy,boki=0;
+    logo();
+    menu();
+        while (juz_zapisane<3)
         {
+        cout<<" Jak narazie zadeklarowales "<<juz_zapisane<<" danych\n";
+        cout<<katy<<" katow i "<<boki<<" bokow\n";
             cout<<"Wybierz co chcesz zapisac (k aby zapisac kat, b aby zapisac bok): ";kolor=9;kolory();cin>>wybor;
             switch(wybor)
             {
@@ -63,37 +73,55 @@ void menu()
                 case 'a':
                 case 'A':
                     informacja();cout<<"Wybrales przypisanie wartosci katowi alfa\n Wpisz wartosc: ";kolor=9;kolory();cin>>alfa;
-                    juz_zapisane+=1;zadeklarowane_katy+=1;
+                    juz_zapisane+=1;katy+=1;
                     break;
                 case 'b':
                 case 'B':
                     informacja();cout<<"Wybrales przypisanie wartosci katowi beta\n Wpisz wartosc: ";kolor=9;kolory();cin>>beta;
-                    juz_zapisane+=1;zadeklarowane_katy+=1;
+                    juz_zapisane+=1;katy+=1;
                     break;
                 case 'g':
                 case 'G':
                     informacja();cout<<"Wybrales przypisanie wartosci katowi gamma\n Wpisz wartosc: ";kolor=9;kolory();cin>>gamma;
-                    juz_zapisane+=1;zadeklarowane_katy+=1;
+                    juz_zapisane+=1;katy+=1;
                     break;
                 default:
                     kolor=12;kolory();cout<<"Nie ma takiej opcji\n";
                     break;
                 }//swithc
+                break;
+            case 'b':
+            case 'B':
+                informacja(); cout<<"Wybrales zapisanie boku\n Wpisz ktoremu bokowi chcesz przypisac wartosc (a, b lub c): ";
+                kolor=9;kolory();cin>>wybor3;
+                switch(wybor3)
+                {
+                case 'a':
+                case 'A':
+                    informacja();cout<<"Wybrales przypisanie wartosci bokowi a\n Wpisz wartosc: ";kolor=9;kolory();cin>>bok_a;
+                    juz_zapisane+=1;boki+=1;
+                    break;
+                case 'b':
+                case 'B':
+                    informacja();cout<<"Wybrales przypisanie wartosci bokowi b\n Wpisz wartosc: ";kolor=9;kolory();cin>>bok_b;
+                    juz_zapisane+=1;boki+=1;
+                    break;
+                case 'c':
+                case 'C':
+                    informacja();cout<<"Wybrales przypisanie wartosci bokowi c\n Wpisz wartosc: ";kolor=9;kolory();cin>>bok_c;
+                    juz_zapisane+=1;boki+=1;
+                    break;
+                default:
+                    kolor=12;kolory();cout<<"Nie ma takiej opcji\n";
+                    break;
+                }//swithc
+            break;
+            default:
+                    kolor=12;kolory();cout<<" Nie ma takiej opcji\n"; break;
             }//swtich
-        
-        }//if
-        else
-    }
 
-}
-int main()
-{
-    logo();
-    while (true)
-    {
-    menu();
+        }
 
-    }
 
     return 0;
 }
